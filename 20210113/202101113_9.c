0e3b4f4518677c3af70644912a303854d6fc12b2
GerganaLp <77080812+GerganaLp@users.noreply.github.com>
2021-01-14 02:46:00 +0200

    Update 202101113_9.c

--- 20210113/202101113_9.c
+++ 20210113/202101113_9.c
@@ -8,8 +8,8 @@ float a=4.9876543;
  double b=7.123456789012345678890;
   printf("%lf\n", b);
  double c=18398458438583853.28;
-  printf("%lf\n", a);
+  printf("%lf\n", c);
   long double d=18398458438583853.39875937284928422;
-   printf("%lf\n", a);
+   printf("%lf\n", d);
   return 0;
 }
