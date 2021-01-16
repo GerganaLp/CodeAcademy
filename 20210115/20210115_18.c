341dc85adcf2302ebdfaf118e240ec5316442e4c
GerganaLp <77080812+GerganaLp@users.noreply.github.com>
2021-01-16 11:37:58 +0200

    Update 20210115_18.c

--- 20210115/20210115_18.c
+++ 20210115/20210115_18.c
@@ -1,55 +1,24 @@
-#include <stdio.h>
+#include <stdio.h> 
+
 int main(){
-unsigned char kilos; // za kilogram 
-unsigned char piece;// za broi
-float total;
- float tomatoes=4.5;
- float totalTomatoes;
- float flour=1.8;
- float totalFlour;
- float milk=0.5;
- float totalMilk;
- float ice_cream=0.6;
- float total_ice;
- float candy=1.5;
- float totalCandy;
- float lollipop=0.15;
- float totalLoli;
-printf("Hello, welcome to our shop. We have tomatoes, flour, milk, ice cream, candy and lollipop\n ");
-printf("How many kilograms of tomatoes do you want? ");
-scanf("%d",&kilos);
-totalTomatoes=kilos*tomatoes;
-printf("%2.f\n" ,totalTomatoes);
-
-printf("How many kilograms of flour do you want? ");
-scanf("%d",&kilos);
-totalFlour=kilos*flour;
-printf("%2.f\n" ,totalFlour);
-
-
-printf("How much milk do you want? ");
-scanf("%d",&piece);
-totalMilk=piece*milk;
-printf("%2.f\n" ,totalMilk);
-
-
-printf("How many ice creams do you want? ");
-scanf("%d",&piece);
-total_ice=piece*ice_cream;
-printf("%2.f\n" ,total_ice);
-
-
-printf("How many candies do you want? ");
-scanf("%d",&piece);
-totalCandy=piece*candy;
-printf("%2.f\n" ,totalCandy);
-
-
-printf("How many lollipops of flour do you want? ");
-scanf("%d",&piece);
-totalLoli=piece*lollipop;
-total=totalLoli+totalMilk+totalTomatoes+totalFlour+totalCandy;
-printf("Your order pays: %2.f\n" ,total);
+    unsigned int brKaravani, brKemperi;
+    int priceKaravana = 90, priceKemper = 100, choice, sum;
+    int days;
+    printf("Dobre doshli v Karavan i Kempeti pod naem\n");
+    printf("Ako zelaete karavana natisnete 1\n");
+    printf("Ako zelaete kemper natisnete 2\n");
+    scanf("%d",&choice);
+    if(choice==1){   
+    printf("Molq vuvedete za kolko dni shte naemete.\n");
+    scanf("%d", &days);
+    printf("Vie izbrahte da naemete karavana za %d dni i struva obshto %d lv ",days,days*priceKaravana);
+   }
+   else if(choice==2){
+    printf("Molq vuvedete za kolko dni shte naemete.\n");
+    scanf("%d", &days);
+    printf("Vie izbrahte da naemete kemper za %d dni i struva obshto %d lv ",days,days*priceKemper);
+   }
+   
 
     return 0;
 }
