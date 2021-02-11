9570c82f7b78b913932053ae5e81d53634a5f78e
GerganaLp <77080812+GerganaLp@users.noreply.github.com>
2021-02-11 20:19:05 +0200

    Create 20210210_4.c

---
+++ 20210210/20210210_4.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>  
+#include <time.h>
+
+int main(void) 
+{
+    srand((unsigned int)(time(NULL)));
+    int i;
+    char pass[13]; 
+
+    printf("Press enter to get a twelve-character password\n");
+    getchar();
+
+    for (i = 0; i < 4; i++) 
+    {
+        
+        pass[ 3 * i ] = '0' + (rand() % 10);
+        char capLetter = 'A' + (rand() % 26); 
+        pass[(3 * i) + 1] = capLetter;
+        char letter = 'a' + (rand() % 26); 
+        pass[(3 * i) + 2] = letter;
+    }
+    pass[3 * i] = '\0';
+    printf("generated password : %s\n\n",pass); 
+
+    printf("\n\n");
+}
