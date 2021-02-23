5c1a1e96310dc4cd252f982ad3dd98d91c2fec47
GerganaLp <77080812+GerganaLp@users.noreply.github.com>
2021-02-23 13:23:31 +0200

    Create 20210222_15.c

---
+++ 20210222/20210222_15.c
@@ -0,0 +1,33 @@
+/*Задача 15. Дефинирайте структура с 4 полета (int. char[10], double, enum),
+направете масив от 20 структури и ги попълнете. Изведете масива
+от структури на стандартния изход в CSV формат (
+https://en.wikipedia.org/wiki/Comma-separated_values ). Пример:
+prog1 > structs20.cvs*/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+#define LENGTH 20
+
+enum grade{
+    num,
+};
+struct Student{
+    int fn;
+    char name[10];
+    double average;
+    enum grade number;
+};
+int main(){
+    struct Student arr[20];
+    for(int i=0; i<20; i++){
+        arr[i].fn=i+1;
+        arr[i].average=(double)i+1.5;
+        strcpy(array[i].str,"good");
+       
+        printf("%d,%s,%lf,%d\n",arr[i].fn,arr[i].name,arr[i].average,arr[i].number);
+    }
+
+    return 0;
+}
